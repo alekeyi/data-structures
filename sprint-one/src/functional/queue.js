ef867451238d0b15b10fd55ef12d0f5f7cd4448e
alekeyi <46357574+alekeyi@users.noreply.github.com>
2019-05-31 08:52:02 -0700

    Finished functional section

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -5,14 +5,49 @@ var Queue = function() {
   var storage = {};
 
   // Implement the methods below
-
+  var counter = 0;
   someInstance.enqueue = function(value) {
+    storage[counter] = value;
+    // if (storage[0]) {
+    //   for (var key in storage){
+    //     if (key > highest) {
+    //       highest = key;
+    //       storage[highest + 1] = value;
+    //     }
+    //   }
+
+
+    // } else {
+    //   storage[0] = value;
+    // }
+    counter++;
   };
 
   someInstance.dequeue = function() {
+    var poppedVal = {}
+
+    if (storage[0]) {
+      poppedVal.result = storage[0];
+      storage[0] = undefined;
+
+      for (var key in storage) {
+        storage[key - 1] = storage[key];
+      }
+    if (counter > 0){
+      counter--
+    }
+    return poppedVal.result;
+    }
+
+
+
+
+
+
   };
 
   someInstance.size = function() {
+    return counter;
   };
 
   return someInstance;
