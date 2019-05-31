d5165d0f4dd39dbfb497ab3c5032582594ddacbb
alekeyi <46357574+alekeyi@users.noreply.github.com>
2019-05-31 08:51:15 -0700

    Finished function-shared section

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,39 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  // output: object
+  // constraints: no prototype, no new, use method t_his, and _.extend
+  // edge cases: empty objects
+  var someInstance = {};
+  someInstance.counter = 0;
+  someInstance.storage = {};
+  _.extend(someInstance, queueMethods);
+  return someInstance;
 };
 
-var queueMethods = {};
+var queueMethods = {enqueue: function(value){
+  this.storage[this.counter] = value;
+  this.counter++;
+},
+
+dequeue: function(){
+  var poppedVal = {};
+  if (this.storage[0]) {
+      poppedVal.result = this.storage[0];
+      this.storage[0] = undefined;
+
+      for (var key in this.storage) {
+        this.storage[key - 1] = this.storage[key];
+      }
+      if (this.counter > 0){
+        this.counter--
+      }
+    return poppedVal.result;
+    }
+},
+
+size: function(){
+  return this.counter;
+}};
 
 
--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,36 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  // input: object
+  // output: value
+  // constraints: no prototype, no new, use method t_his, and _.extend
+  // edge cases: empty objects
+  var someInstance = {};
+  someInstance.counter = 0
+  someInstance.storage = {};
+  _.extend(someInstance, stackMethods);
+  return someInstance;
 };
 
-var stackMethods = {};
 
 
+var stackMethods = {push: function(value){
+    this.storage[this.counter] = value;
+    this.counter++;
+  },
+  pop: function(){
+    if(this.counter > 0){
+      this.counter--;
+    }
+    return this.storage[this.counter];
+  },
+  size: function(){
+    return this.counter;
+  }};
+
+
+// This test could fail if the word "this" is found inside comments,
+// // or if there are opening/closing braces ("{", "}") in comments inside the constructor.
+
+// var constructorStr = patternIs('es6') ? extractClassConstructor(constructor) : constructor.toString();
+// assuming(referencesThis).expect(/((?!\/\/).)*(this)/m.test(constructorStr)).to.be.true;
