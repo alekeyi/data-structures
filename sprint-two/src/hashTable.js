0ebf11602f1a051d2e6b64daf30a4be39ef174e6
alekeyi <aleksey.ioseliani@gmail.com>
2019-05-31 20:05:40 -0500

    FINISHED HASHTABLE !!!!!!!??????

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -7,14 +7,55 @@ var HashTable = function() {
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  // if storage[index] is empty
+    //insert v
+  //if storage[index is not empty]
+    //create tuple
+  var tuple = [k, v]
+  if(this._storage[index] === undefined){
+    this._storage[index] = [tuple];
+    
+  }else if (this._storage[index][0][0] === tuple[0]) {
+ 
+    //check for same k
+      //if it's the same
+      this._storage[index][0][1] = tuple[1]
+        //overwrite it somehow
+  }else{
+    
+    this._storage[index].push(tuple)  
+  }
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  //iterate through storage
+  
+  for (var i = 0; i < this._storage[index].length; i++){
+    
+    //if storage[i][0] is the same as target
+    if (this._storage[index][i][0] === k){
+      //return storage[i][1]
+     
+      return this._storage[index][i][1];
+      }
+    }
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  //iterate through storage[index]
+  for (var i = 0; i < this._storage[index].length; i++){
+    
+    //if storage[index][i] matches the target
+    if (this._storage[index][i][0] === k){
+      console.log(this._storage[index][i][0])
+      //get rid of it
+      this._storage[index].splice(i, 1);
+      }
+    }
+
+
 };
 
 
--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,18 +1,23 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = null; // fix me
+  this._storage = []; // fix me
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+  if(!_storage.includes(item)){
+    _storage.push(item);
+  }
 };
 
 setPrototype.contains = function(item) {
+  return (_storage.includes(item));
 };
 
 setPrototype.remove = function(item) {
+  _storage.splice(_storage.indexOf(item), 1);
 };
 
 /*
